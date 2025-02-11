763784346138f7b4a56de0e256f8c67683ad2ae3
Pratyush Birole <birole.pratyush@gmail.com>
2025-02-11 15:33:55 +0530

    Fixed Utils/index.js imports

--- Utils/sendEmail.js
+++ Utils/sendEmail.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import { v4 as uuidv4 } from "uuid";
-import { hashString } from "./index.js";
+import { hashString } from "../Utils/index.js";
 import Verification from "../models/emailVerification.js";
 import PasswordReset from "../models/PasswordReset.js";
 
